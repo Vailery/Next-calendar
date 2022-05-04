fdb0fd975be1d8e0dfd6648261c4d241b5414ea6
Vailery <leraliabik8@gmail.com>
2022-05-04 17:53:19 +0300

    feat: add dark mode

--- next-calendar/components/layout/layout.tsx
+++ next-calendar/components/layout/layout.tsx
@@ -1,6 +1,7 @@
+import Head from "next/head";
 import { Fragment, ReactNode, useContext } from "react";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyles, ThemeContext, ThemeProdiver } from "../../context/theme";
+import { GlobalStyles, ThemeContext } from "../../context/theme";
 
 interface ILayout {
   children: ReactNode;
@@ -8,15 +9,20 @@ interface ILayout {
 
 function Layout({ children }: ILayout) {
   const { theme } = useContext(ThemeContext);
+  console.log(theme);
 
   return (
     <Fragment>
-      <ThemeProdiver>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <main>{children}</main>
-        </ThemeProvider>
-      </ThemeProdiver>
+      <Head>
+        <link rel="icon" href="/assets/img/favicon.ico" />
+        <title>Calendar</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <main>{children}</main>
+      </ThemeProvider>
     </Fragment>
   );
 }
--- next-calendar/pages/_app.tsx
+++ next-calendar/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/layout";
+import { ThemeProdiver } from "../context/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProdiver>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProdiver>
   );
 }
 
